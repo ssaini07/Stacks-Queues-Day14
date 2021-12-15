21c651805e1bc495d67bf1d3e7fa4540900998ca
ssaini07 <78027352+ssaini07@users.noreply.github.com>
2021-12-15 20:46:10 +0530

    Welcome message

---
+++ src/com/bridgelabz/StacksQueues/stackUsingLinkedList.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.StacksQueues;
+
+public class stackUsingLinkedList {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Stacks and Queues");
+
+	}
+}
